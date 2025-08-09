fd5beb6613f3c5eaffb004811fee6fbb4fd33899
ShreyashPrateek <shreyashprateek01@gmail.com>
2025-08-09 22:08:05 +0530

    Creating Tournament Page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import ContactPage from './components/ContactPage';
 import AboutUs from './components/AboutUs';
+import TournamentPage from './components/TournamentPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path="/tournament" element={<TournamentPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </BrowserRouter>
